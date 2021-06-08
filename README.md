4f565305daa4e33108b9f372432a13f93e0b8ad0
Your Name <you@example.com>
2021-06-08 16:48:31 -0400

    Final Updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
 
   ## Table of Contents :books:
-   1. [Description](#description-:clipboard:)
+   1. [Description](#description)
    2. [Installation](#installation)
    3. [Project Usage](#Project-Usage)
    4. [License Information](#License-Information)
@@ -19,13 +19,12 @@
   The following steps were completed in order to develop this application:
   1) Git repository was created which included the gitignore file (for node_modules and .DS_Store especially) and MIT License.
   2) Npm Init install was carried out to develop the package.json.  NPM packages installed & utilized for this project included the FS module, Express, and UUID.
-  3) The Express Server and Initial PORT were they built out, in addition to the middleware necessary for this project.
+  3) The Express Server and Initial PORT were they built out, in addition to the middleware necessary for the JSON data to handle the parsing when getting the POST request.
   4) The Express Listener (located at the base of the server.js file) was there constructed (to actually begin the note taker server).
-  4) HTML Display Routes
-  5) API Routes 
-  6) API Post Route 
-  7) App.Delete Note functionality - still in progress
-  8) 
+  4) HTML Display Routes - 2 seperate HTML Routes were created using "app.get" to return the index.html and notes.html files.
+  5) API (GET) Route as then built to handle the reading of the db.json file (notes written in by the user), and then returning that inputted data as saved notes in JSON format. 
+  6) API (POST) Route was then created to save the users' notes and return the data to them (in the saved left column of the application).
+  7) API (DELETE) Route was finally constructed which uses the FILTER FS method to go through the user's submitted noted by ID, and then delete accordinagly (based off the trash can visual on the application).
 
   ## Project Usage :pencil2:
   This project's purpose was to create a tool for any business owner or client, whom wants to keep track of their notes on an ongoing basis, while having them saved with the option to have an ongoing delete functionality option as well.  The project was to be hosted through a app via Heroku, so it could be accessed anywhere, while utilizing backend programming technology.
@@ -38,10 +37,13 @@
 
   ## Testing Instructions :man_scientist:
   Follow these steps:
+  1) Utilizing gitbash or the termianl, the project folder needs to be opened locally and the node server.js call must first be run, to establish the connection to the PORT.
+  2) You can copy/paste the URL to go to the local web application to see functionality (pictured below).
+  3) Heroku - Follow this link to access the application via the web:
 
   ## Further Development :man_technologist:	
-  To be developed
-
+  * More dry coding
+  * Visuals could be cleaner
   
   ## Questions? :raising_hand_man:
   ### Please find my contact information below to reach out! 
--- server.js
+++ server.js
@@ -2,60 +2,67 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const uuid = require('uuid');
+const uniqid = require('uniqid');
 
 // Creating express server & setting initial port 
 const app = express();
-const PORT = process.env.PORT || 0825;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for JSON data to handle parsing when get POST request
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
 
 // HTML Display Routes
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
-
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
 // API Route to read db.json file & return saved notes as JSON
 app.get("/api/notes", (req, res) => {
     fs.readFile("db/db.json", "utf8", (error, data) => {
-        if (error) throw error
-        res.json(data)
+        if (error) {
+            console.error(error)
+        } else {
+            res.send(data);
+        }
     })
 });
 
 // (Post) API Route to save user's submitted notes and return to client
 app.post("/api/notes", (req, res) => {
-    const newNote = req.body
     fs.readFile("db/db.json", "utf8", (error, data) => {
-        if (error) throw error
-        let notes = data
-        notes.push(newNote)
-        // Writing the data array to the db.json file
-        fs.writeFile("db/db.json", data, (error) => {
-            if (error)
-                console.log(err);
-            else {
-                console.log(fs.readFileSync("db.db/json", "utf8"));
-            }
-        })
-        res.send(res.json.newNote)
-    });
+        if (error) {
+            console.error(error)
+        }
+        else {
+            let notes = JSON.parse(data);
+            console.log(notes);
+            const newNote = req.body
+            newNote.id = uniqid();
+            notes.push(newNote);
+            fs.writeFile("db/db.json", JSON.stringify(notes), (err) =>
+                err ? console.error(err) : console.log('New Note Saved!'))
+            res.send(newNote);
+        };
+    })
+
 });
 
-// // Delete Notes functionality - Need to FILTER through process to remove notes - 6.5.21
-// app.delete("/api/notes:id", (req, res) => {
-//     fs.readFile("db/db.json", "utf8", (error, data) => {
-//         if (error) throw error
-//         let notes = JSON.parse(data);
-//         notes.filter(newNote) (req.parm.id)
-//         fs.writeFile()
-//      
-//     }});
-
-// In case API Route don't function place here
-// app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+// // Delete Notes functionality - FILTERS through user's submitted data (through JSON parsing) to process and remove notes 
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("db/db.json", "utf8", (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        else {
+            let notes = JSON.parse(data);
+            notes = notes.filter(e => e.id != req.params.id)
+            fs.writeFile("db/db.json", JSON.stringify(notes), (err) =>
+                err ? console.error(err) : console.log('Note Deleted!'))
+            res.send({});
+        }
+    })
+})
 
 // Express "listener" to start the note taker server
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
